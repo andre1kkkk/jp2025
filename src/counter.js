6c956c1c9deae0fa5e84fdac0aa5e228e8e2df18
andre1kkkk <sempreprofissional12@gmail.com>
2025-09-02 17:56:15 -0300

    upgrades

--- src/counter.js
+++
@@ -1,9 +0,0 @@
-export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `conte é ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter - 10))
-  setCounter(20)
-}
